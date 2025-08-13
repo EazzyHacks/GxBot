cbdfa4dac76e25c80879f6e77bc776dbbb45f314
EazzyHacks <yhair7200@gmail.com>
2025-08-13 06:06:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
 function verify() {
-  const dirs = ['tmp', 'Sesiones/Subbots', 'Sesiones/Principal'];
+  const dirs = ['tmp', 'Subbots', 'Principal'];
   for (const dir of dirs) {
     if (typeof dir === 'string' && dir.trim() !== '') {
       if (!existsSync(dir)) {
@@ -29,14 +29,14 @@ function verify() {
 verify();
 
 // Diseño para "Sasuke Bot"
-say('sᥲsᥙkᥱ ᑲ᥆𝗍', {
+say('Gx Bot', {
   font: 'block', // Estilo 'block' para un diseño fuerte
   align: 'center',
   colors: ['red', 'white'], // Colores que recuerdan al Sharingan
   background: 'black' // Fondo oscuro para resaltar el texto
 });
 
-say(`Developed By • sᥲsᥙkᥱ ᑲ᥆𝗍 mძ`, {
+say(`Developed By evolution`, {
   font: 'console',
   align: 'center',
   colors: ['magenta']
